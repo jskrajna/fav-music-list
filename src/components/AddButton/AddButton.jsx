73c37c62b3dffac44b1a5f2f1e8c5697030cea8b
Justyna Skrajna <jskrajna@gmail.com>
2022-11-07 18:36:14 +0400

    remove unused things from AddButton component

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -1,4 +1,3 @@
-import { PlusCircleIcon } from '@heroicons/react/24/outline'
 const AddButton = ({ onClick, children, ...rest }) => {
     return (
         <button onClick={onClick} {...rest}>
@@ -8,6 +7,3 @@ const AddButton = ({ onClick, children, ...rest }) => {
 };
 
 export default AddButton;
-
-// takie entry już istnieje
-// nie można pustego dodać
